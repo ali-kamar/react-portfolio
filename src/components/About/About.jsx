d35513fcd11e72ad726d2fde044f34d407a8fe47
Ali Kamar <alykamarr@gmail.com>
2024-08-16 16:14:00 +0300

    finish about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { HERO_CONTENT } from "../../constants";
-import profilePic from "../../assets/Me.png"
+import profilePic from "../../assets/Me.png";
 import { motion } from "framer-motion";
 
 const About = () => {
@@ -14,16 +14,43 @@ const About = () => {
         >
           Full Stack Developer
         </span>
-        <p className="py-6 my-2  max-w-xl font-light tracking-tighter">{HERO_CONTENT}</p>
+        <p className="py-6 my-2 max-w-xl font-light tracking-tighter">
+          {HERO_CONTENT}
+        </p>
       </div>
-      <div className="">
-        <div className="flex justify-center">
-          <img src={profilePic} alt="" className="w-96" />
+      <div className="relative">
+        <div className="flex justify-center items-center">
+          <motion.svg
+            className="w-[300px] xl:w-[406px] h-[300px] xl:h-[506px] absolute"
+            fill="transparent"
+            viewBox="0 0 506 506"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <motion.circle
+              cx="253"
+              cy="253"
+              r="250"
+              stroke="#8b5cf6" 
+              strokeWidth="4"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              initial={{ strokeDasharray: "24 10 0 0" }}
+              animate={{
+                strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
+                rotate: [120, 360],
+              }}
+              transition={{
+                duration: 20,
+                repeat: Infinity,
+                repeatType: "reverse",
+              }}
+            />
+          </motion.svg>
+          <img src={profilePic} alt="Ali Kamar" className="w-96 relative z-10" />
         </div>
       </div>
     </div>
   );
 };
 
-export default About;
 export default About;
