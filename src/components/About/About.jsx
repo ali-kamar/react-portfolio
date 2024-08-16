4a70a1e42163bd932583808c3c5c2d157f394bdf
Ali Kamar <alykamarr@gmail.com>
2024-08-16 16:29:20 +0300

    apply animation to about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,24 +3,50 @@ import { HERO_CONTENT } from "../../constants";
 import profilePic from "../../assets/Me.png";
 import { motion } from "framer-motion";
 
+const container = (delay) => ({
+  hidden: { x: -100, opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: { duration: 0.5, delay: delay },
+  },
+});
 const About = () => {
   return (
-    <div className="container flex justify-between ">
+    <div className="container flex justify-around ">
       <div className="Left flex flex-col">
-        <h2 className="pb-16 text-6xl font-thin tracking-wider">Ali Kamar</h2>
-        <span
+        <motion.h2
+          variants={container(0)}
+          initial="hidden"
+          animate="visible"
+          className="pb-16 text-6xl font-thin tracking-wider"
+        >
+          Ali Kamar
+        </motion.h2>
+        <motion.span
+          variants={container(0.5)}
+          initial="hidden"
+          animate="visible"
           className="bg-gradient-to-r from-purple-500 via-slate-500 to-white 
          text-transparent text-4xl tracking-tight bg-clip-text"
         >
           Full Stack Developer
-        </span>
-        <p className="py-6 my-2 max-w-xl font-light tracking-tighter">
+        </motion.span>
+        <motion.p
+          variants={container(1)}
+          initial="hidden"
+          animate="visible"
+          className="py-6 my-2 max-w-xl font-light tracking-tighter"
+        >
           {HERO_CONTENT}
-        </p>
+        </motion.p>
       </div>
       <div className="relative">
         <div className="flex justify-center items-center">
           <motion.svg
+            variants={container(1.2)}
+            initial="hidden"
+            animate="visible"
             className="w-[300px] xl:w-[406px] h-[300px] xl:h-[506px] absolute"
             fill="transparent"
             viewBox="0 0 506 506"
@@ -30,7 +56,7 @@ const About = () => {
               cx="253"
               cy="253"
               r="250"
-              stroke="#8b5cf6" 
+              stroke="#8b5cf6"
               strokeWidth="4"
               strokeLinecap="round"
               strokeLinejoin="round"
@@ -46,7 +72,14 @@ const About = () => {
               }}
             />
           </motion.svg>
-          <img src={profilePic} alt="Ali Kamar" className="w-96 relative z-10" />
+          <motion.img
+            variants={container(1.2)}
+            initial="hidden"
+            animate="visible"
+            src={profilePic}
+            alt="Ali Kamar"
+            className="w-96 relative z-10"
+          />
         </div>
       </div>
     </div>
