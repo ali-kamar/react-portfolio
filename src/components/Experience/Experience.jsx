8fa6d1e2f70829d8bdb7cabead2a867ba30030a2
Ali Kamar <alykamarr@gmail.com>
2024-08-18 15:37:55 +0300

    fix a bug

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -19,7 +19,7 @@ const Experience = () => {
             whileInView={{ opacity: 1, x: 0 }}
             initial={{ opacity: 0, x: -100 }}
             transition={{ duration: 1.5 }}
-            className="w-full"
+            className="sm:w-1/4 xxs:w-full"
           >
             <p className="text-sm mb-2 text-white dark:text-black">
               {job.year}
